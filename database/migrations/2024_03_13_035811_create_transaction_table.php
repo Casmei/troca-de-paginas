b07d8d12cb241f350e71c6bdba034854f5ba668e
Tiago de Castro Lima <casmei@protonmail.com>
2024-03-13 01:05:02 -0300

    feat: add transactions column

---
+++ database/migrations/2024_03_13_035811_create_transaction_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('requester_id');
+            $table->foreign('requester_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')->references('id')->on('books');
+
+            $table->enum('status', [
+                'pending',
+                'approved',
+                'completed',
+                'rejected',
+                'canceled',
+                'returned',
+                'received',
+                'in_progress',
+            ]);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
