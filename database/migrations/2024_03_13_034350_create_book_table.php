0f7fd96acacc8242929d51aec0ef13b3a4214f0a
Tiago de Castro Lima <casmei@protonmail.com>
2024-03-13 00:58:33 -0300

    feat: add new book table

---
+++ database/migrations/2024_03_13_034350_create_book_table.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owner_id');
+            $table->string('title');
+            $table->string('author');
+            $table->longText('description');
+            $table->enum('conservation_state', [
+                'new',
+                'excellent',
+                'good',
+                'regular',
+                'worn',
+                'damaged',
+                'unusable',
+            ]);
+            $table->enum('gender', [
+                'fiction',
+                'romance',
+                'suspense',
+                'horror',
+                'fantasy',
+                'science_fiction',
+                'drama',
+                'action',
+                'adventure',
+                'mystery',
+                'history',
+                'biography',
+                'self_help',
+                'poetry',
+                'short_story',
+                'chronicle',
+                'essay',
+                'educational',
+                'religion',
+                'cooking',
+                'comic_book',
+                'manga',
+                'children_literature',
+            ]);
+            $table->enum('transacation_type', [
+                'donation',
+                'borrow',
+                'exchange',
+            ]);
+            $table->timestamps();
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book');
+    }
+};
