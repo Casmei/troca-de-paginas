e8eb51341931c593cec678bc12e3a8de99f9032e
Tiago de Castro Lima <casmei@protonmail.com>
2024-03-14 00:41:30 -0300

    feat: add toastr into application

---
+++ config/toastr.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Toastr options
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify the options that will be passed to the toastr.js
+    | library. For a full list of options, visit the documentation.
+    |
+    */
+
+    'options' => [
+        'positionClass' => 'toast-bottom-right',
+    ],
+];
