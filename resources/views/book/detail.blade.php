e1807b850c357dcdf76b3afb317f74058ed313ec
Tiago de Castro Lima <casmei@protonmail.com>
2024-03-15 20:24:14 -0300

    feat: add pill for inform book already requested

--- resources/views/book/detail.blade.php
+++ resources/views/book/detail.blade.php
@@ -1,7 +1,7 @@
 <x-index-layout>
-    <div class="    space-y-4 items-center">
-        <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex  focus:outline focus:outline-2 focus:outline-red-500">
-            <div role="status" class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
+    <div class="space-y-4 items-center">
+        <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none  focus:outline focus:outline-2 focus:outline-red-500">
+            <div role="status" class="space-y-8 mb-4 md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
                 <div class="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
                     <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                         <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
@@ -20,14 +20,21 @@
                 </div>
                 <span class="sr-only">Loading...</span>
             </div>
+            <div class="flex justify-end">
+                @if ($transactionRequested)
+                <span class=" bg-red-100 animate-pulse text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
+                    Transaction Already Resquested
+                </span>
+                @endif
+            </div>
         </div>
         <form action="{{route('book.request', ['id' => $book->id])}}" method="post">
             @csrf
 
             <div class="scale-100 p-6 space-x-4 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex  focus:outline focus:outline-2 focus:outline-red-500">
-                <x-primary-button class="bg-indigo-600 hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-800 text-white">
+                <x-primary-button class="bg-indigo-600 enabled:hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-800 text-white disabled:opacity-45">
                     Solicitar empréstimo
-                </x-secondary-button>
+                </x-primary-button>
                 <x-secondary-button>
                     Biblioteca do usuário
                 </x-secondary-button>
